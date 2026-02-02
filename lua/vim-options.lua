5d72fa2c41901815c1c5928ee6fa5c98254c2a8b
Max Alt <max0102.alt@gmail.com>
2026-02-02 23:08:31 +0100

    Updated vim-options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,4 +4,6 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
-vim.opt.clipboard="unnamedplus"
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.number = true
